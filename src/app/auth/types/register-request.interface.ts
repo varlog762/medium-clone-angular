a99d7e4c1ea8620212a5aea33f94ba63537dfcfe
Gregory Pereverzev <boot762@gmail.com>
2024-04-03 20:39:11 +0300

    feat: add AuthStateInterface

--- src/app/auth/types/register-request.interface.ts
+++ src/app/auth/types/register-request.interface.ts
@@ -1,5 +1,7 @@
 export interface RegisterRequestInterface {
-  username: string;
-  email: string;
-  password: string;
+  user: {
+    username: string;
+    email: string;
+    password: string;
+  };
 }
