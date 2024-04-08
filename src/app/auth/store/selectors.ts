d770e516e549e4679b5747491d34379fe3273c03
Gregory Pereverzev <boot762@gmail.com>
2024-04-08 19:01:42 +0300

    feat: add AppStateInterface

--- src/app/auth/store/selectors.ts
+++ src/app/auth/store/selectors.ts
@@ -1,2 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
+import { AppSateInterface } from '../../shared/types/app-sate.interface';
+
+const authFeatureSelector = createFeatureSelector<AppSateInterface>('auth');
