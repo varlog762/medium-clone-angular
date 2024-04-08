d016de0c4b48ba4c656d1bc9f84543a9d6446c73
Gregory Pereverzev <boot762@gmail.com>
2024-04-08 23:22:00 +0300

    feat: add initializeValues method to RegisterComponent

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { Store, StoreModule } from '@ngrx/store';
+import { Store, StoreModule, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import { registerAction } from '../../store/actions/register.action';
+import { authFeature } from '../../store/auth.feature';
 
 @Component({
   selector: 'mc-register',
@@ -15,11 +17,19 @@ import { registerAction } from '../../store/actions/register.action';
 })
 export class RegisterComponent implements OnInit {
   public form!: FormGroup;
+  public isSubmitting$!: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(
+      select(authFeature.selectIsSubmitting)
+    );
   }
 
   initializeForm(): void {
