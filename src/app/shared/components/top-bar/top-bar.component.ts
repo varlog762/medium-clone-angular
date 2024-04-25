3bbf38b6480113a88eb557ef35d11d9a032624c8
Gregory Pereverzev <boot762@gmail.com>
2024-04-25 11:31:32 +0300

    feat: add TopBarComponent

--- src/app/shared/components/top-bar/top-bar.component.ts
+++ src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 import { Store } from '@ngrx/store';
@@ -9,17 +10,20 @@ import { authFeature } from '../../../auth/store/auth.feature';
 @Component({
   selector: 'mc-top-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
 export class TopBarComponent implements OnInit {
-  public isLoggedIn$!: Observable<boolean>;
+  public isLoggedIn$!: Observable<boolean | null>;
+  public isAnonymous$!: Observable<boolean>;
   public currentUser$!: Observable<CurrentUserInterface | null>;
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.store.select(authFeature.selectIsLoggedIn);
+    this.isLoggedIn$ = this.store.select(authFeature.selectIsLoggedIn);
+    this.isAnonymous$ = this.store.select(authFeature.selectIsAnonymous);
+    this.currentUser$ = this.store.select(authFeature.selectCurrentUser);
   }
 }
