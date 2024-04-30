eadce0f2cd32f87f1bc1cf3c34a5c6d6c87065cb
Gregory Pereverzev <boot762@gmail.com>
2024-04-30 18:52:51 +0300

    feat: add GetFeedResponseInterface

--- src/app/global-feed/components/global-feed/global-feed.component.ts
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { FeedComponent } from '../../../shared/components/feed/feed.component';
+
 @Component({
   selector: 'mc-global-feed',
   standalone: true,
-  imports: [],
+  imports: [FeedComponent],
   templateUrl: './global-feed.component.html',
   styleUrl: './global-feed.component.scss',
 })
---
+++ src/app/shared/types/get-feed-response.interface.ts
@@ -0,0 +1,4 @@
+export interface GetFeedResponseInterface {
+  articles: ArticleInterface[];
+  articleCount: number;
+}
