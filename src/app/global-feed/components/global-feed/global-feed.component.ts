38e905d823272486fe90c12c4987d868d7205915
Gregory Pereverzev <boot762@gmail.com>
2024-04-30 19:15:34 +0300

    refactor: change the project structure

--- src/app/global-feed/components/global-feed/global-feed.component.ts
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { FeedComponent } from '../../../shared/components/feed/feed.component';
+import { FeedComponent } from '../feed/feed.component';
 
 @Component({
   selector: 'mc-global-feed',
--- src/app/global-feed/types/get-feed-response.interface.ts
+++ src/app/global-feed/types/get-feed-response.interface.ts
@@ -1,4 +1,4 @@
-import { ArticleInterface } from './article.interface';
+import { ArticleInterface } from '../../shared/types/article.interface';
 
 export interface GetFeedResponseInterface {
   articles: ArticleInterface[];
