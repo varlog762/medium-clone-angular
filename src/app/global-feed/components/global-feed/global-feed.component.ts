febf6ab6a40022414607e2655b77276f8ff1f7c5
Gregory Pereverzev <boot762@gmail.com>
2024-04-30 18:29:11 +0300

    feat: add GlobalFeedComponent

---
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-global-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-feed.component.html',
+  styleUrl: './global-feed.component.scss',
+})
+export class GlobalFeedComponent {}
