de6a5b12eb3798e20f86bd8cd16a5a165d14b69e
Gregory Pereverzev <boot762@gmail.com>
2024-04-30 18:39:18 +0300

    feat: add FeedComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { RegisterComponent } from './auth/components/register/register.component';
 import { LoginComponent } from './auth/components/login/login.component';
+import { GlobalFeedComponent } from './global-feed/components/global-feed/global-feed.component';
 
 export const routes: Routes = [
+  { path: '', component: GlobalFeedComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
