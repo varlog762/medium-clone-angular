d3c3529148cc156eb64cd6c234ffb25819ba8d55
Gregory Pereverzev <boot762@gmail.com>
2024-05-02 20:29:21 +0300

    feat: add ErrorMessageComponent

---
+++ src/app/global-feed/components/error-message/error-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-error-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-message.component.html',
+  styleUrl: './error-message.component.scss',
+})
+export class ErrorMessageComponent {}
--- src/app/global-feed/components/global-feed/global-feed.component.ts
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { FeedComponent } from '../feed/feed.component';
+import { BannerComponent } from '../banner/banner.component';
 
 @Component({
   selector: 'mc-global-feed',
   standalone: true,
-  imports: [FeedComponent],
+  imports: [FeedComponent, BannerComponent],
   templateUrl: './global-feed.component.html',
   styleUrl: './global-feed.component.scss',
 })
