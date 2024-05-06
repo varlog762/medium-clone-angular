594c9b2fe237e201df6811d4cebc755ab9374625
Gregory Pereverzev <boot762@gmail.com>
2024-05-07 00:52:45 +0300

    feat: update AppSateInterface

--- src/app/shared/types/app-state.interface.ts
+++ src/app/shared/types/app-state.interface.ts
@@ -1,7 +1,9 @@
 import { AuthStateInterface } from '../../auth/types/auth-state.interface';
 import { FeedStateInterface } from '../../global-feed/types/feed-state.interface';
+import { PopularTagsStateInterface } from '../../global-feed/types/popular-tags-state.interface';
 
 export interface AppSateInterface {
   auth: AuthStateInterface;
   feed: FeedStateInterface;
+  popularTags: PopularTagsStateInterface;
 }
