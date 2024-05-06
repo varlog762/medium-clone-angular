3c56b5a6964396d704bee807e78e5f8727a68bad
Gregory Pereverzev <boot762@gmail.com>
2024-05-07 00:43:06 +0300

    feat: implement PopularTagsStateInterface

--- src/app/global-feed/types/popular-tags-state.interface.ts
+++ src/app/global-feed/types/popular-tags-state.interface.ts
@@ -1,1 +1,6 @@
-export interface PopularTagsStateInterface {}
+import { PopularTagType } from '../../shared/types/popular-tag.type';
+
+export interface PopularTagsStateInterface {
+  tags: PopularTagType[] | null;
+  error: string | null;
+}
