65c705395db0db157975046c0b40da74a07d89ea
Gregory Pereverzev <boot762@gmail.com>
2024-05-07 15:23:44 +0300

    feat: modify PopularTagsComponent class, implement PopularTagsComponent template

--- src/app/global-feed/components/popular-tags/popular-tags.component.ts
+++ src/app/global-feed/components/popular-tags/popular-tags.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/internal/Observable';
 
 import { PopularTagType } from '../../../shared/types/popular-tag.type';
 import { PopularTagsActions } from '../../store/actions/popular-tags.actions';
 import { popularTagsFeature } from '../../store/features/popular-tags.feature';
+import { LoadingComponent } from '../loading/loading.component';
+import { ErrorMessageComponent } from '../error-message/error-message.component';
 
 @Component({
   selector: 'mc-popular-tags',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, LoadingComponent, ErrorMessageComponent, RouterLink],
   templateUrl: './popular-tags.component.html',
   styleUrl: './popular-tags.component.scss',
 })
 export class PopularTagsComponent implements OnInit {
   public tags$!: Observable<PopularTagType[] | null>;
+  public isLoading$!: Observable<boolean>;
+  public error$!: Observable<string | null>;
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.store.dispatch(PopularTagsActions.getPopularTags());
+    this.fetchData();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
     this.tags$ = this.store.select(popularTagsFeature.selectTags);
+    this.isLoading$ = this.store.select(popularTagsFeature.selectIsLoading);
+    this.error$ = this.store.select(popularTagsFeature.selectError);
+  }
+
+  fetchData(): void {
+    this.store.dispatch(PopularTagsActions.getPopularTags());
   }
 }
