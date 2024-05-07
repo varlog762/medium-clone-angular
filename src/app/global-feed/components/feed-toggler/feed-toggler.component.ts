5ad066e8f5d4bb1707c3ad432f4a88a337cd4946
Gregory Pereverzev <boot762@gmail.com>
2024-05-07 15:28:00 +0300

    feat: add FeedTogglerComponent

---
+++ src/app/global-feed/components/feed-toggler/feed-toggler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-feed-toggler',
+  standalone: true,
+  imports: [],
+  templateUrl: './feed-toggler.component.html',
+  styleUrl: './feed-toggler.component.scss',
+})
+export class FeedTogglerComponent {}
