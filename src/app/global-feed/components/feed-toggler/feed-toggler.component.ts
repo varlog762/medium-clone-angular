d5638471e7ff304784c56893f0094a5bf3412b83
Gregory Pereverzev <boot762@gmail.com>
2024-05-07 15:47:19 +0300

    feat: implement FeedTogglerComponent class and template

--- src/app/global-feed/components/feed-toggler/feed-toggler.component.ts
+++ src/app/global-feed/components/feed-toggler/feed-toggler.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs/internal/Observable';
+import { Store } from '@ngrx/store';
+
+import { authFeature } from '../../../auth/store/auth.feature';
 
 @Component({
   selector: 'mc-feed-toggler',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './feed-toggler.component.html',
   styleUrl: './feed-toggler.component.scss',
 })
-export class FeedTogglerComponent {}
+export class FeedTogglerComponent implements OnInit {
+  @Input('tagName') tagName!: string | null;
+
+  public isLoggedIn$!: Observable<boolean | null>;
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.store.select(authFeature.selectIsLoggedIn);
+  }
+}
