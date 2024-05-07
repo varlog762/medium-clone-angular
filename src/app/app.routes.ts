251826d76c9623438821befd0a9db269fc93245a
Gregory Pereverzev <boot762@gmail.com>
2024-05-08 01:17:06 +0300

    feat: add 'tags/:slug' route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { RegisterComponent } from './auth/components/register/register.component
 import { LoginComponent } from './auth/components/login/login.component';
 import { GlobalFeedComponent } from './global-feed/components/global-feed/global-feed.component';
 import { YourFeedComponent } from './your-feed/components/your-feed.component';
+import { TagFeedComponent } from './tag-feed/components/tag-feed.component';
 
 export const routes: Routes = [
   { path: '', component: GlobalFeedComponent },
   { path: 'feed', component: YourFeedComponent },
+  { path: 'tags/:slug', component: TagFeedComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
