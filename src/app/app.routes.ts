b205a6f2911ed89b7a228b4a5ab4b15e4a791033
Gregory Pereverzev <boot762@gmail.com>
2024-05-08 00:57:41 +0300

    feat: add 'feed' route, modify FeedToggleComponent template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { LoginComponent } from './auth/components/login/login.component';
 import { GlobalFeedComponent } from './global-feed/components/global-feed/global-feed.component';
+import { YourFeedComponent } from './your-feed/components/your-feed.component';
 
 export const routes: Routes = [
   { path: '', component: GlobalFeedComponent },
+  { path: 'feed', component: YourFeedComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
