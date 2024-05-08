608047da2d5d0ac673b2aa0949d0f9fe2e7937dc
Gregory Pereverzev <boot762@gmail.com>
2024-05-09 01:17:51 +0300

    feat: implement getArticle method in ArticleService

--- src/app/shared/services/article.service.ts
+++ src/app/shared/services/article.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
+import { GetArticleResponseInterface } from '../types/get-article-response.interface';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,6 +11,6 @@ export class ArticleService {
   constructor(private http: HttpClient) {}
 
   getArticle(slug: string): Observable<GetArticleResponseInterface> {
-    return this.http.get();
+    return this.http.get<GetArticleResponseInterface>(slug);
   }
 }
