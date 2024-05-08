20deb146f6b5e5caed6b8bf5ee481c610501aeee
Gregory Pereverzev <boot762@gmail.com>
2024-05-09 01:21:00 +0300

    feat: modify getArticle method in ArticleService

--- src/app/shared/services/article.service.ts
+++ src/app/shared/services/article.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
 
 import { GetArticleResponseInterface } from '../types/get-article-response.interface';
+import { ArticleInterface } from '../types/article.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +12,9 @@ import { GetArticleResponseInterface } from '../types/get-article-response.inter
 export class ArticleService {
   constructor(private http: HttpClient) {}
 
-  getArticle(slug: string): Observable<GetArticleResponseInterface> {
-    return this.http.get<GetArticleResponseInterface>(slug);
+  getArticle(slug: string): Observable<ArticleInterface> {
+    return this.http
+      .get<GetArticleResponseInterface>(slug)
+      .pipe(map((response: GetArticleResponseInterface) => response.article));
   }
 }
