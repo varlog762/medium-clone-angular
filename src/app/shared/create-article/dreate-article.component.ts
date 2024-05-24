6e58d14990dd8498b06ea74cfa55c8e2f9b6d583
Gregory Pereverzev <boot762@gmail.com>
2024-05-24 19:04:47 +0300

    feat: add CreateArticleComponent

---
+++ src/app/shared/create-article/dreate-article.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-create-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-article.component.html',
+  styleUrl: './create-article.component.scss',
+})
+export class CreateArticleComponent {}
