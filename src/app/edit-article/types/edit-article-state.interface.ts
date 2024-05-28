6f58c907345070863d7fae4c308dd18086c4e5b8
Gregory Pereverzev <boot762@gmail.com>
2024-05-28 16:54:57 +0300

    feat: add EditArticleStateInterface

---
+++ src/app/edit-article/types/edit-article-state.interface.ts
@@ -0,0 +1,8 @@
+import { ArticleInterface } from '../../shared/types/article.interface';
+import { BackendErrorsInterface } from '../../shared/types/backend-errors.interface';
+
+export interface EditArticleStateInterface {
+  isSubmitting: boolean;
+  data: ArticleInterface | null;
+  errors: BackendErrorsInterface | null;
+}
