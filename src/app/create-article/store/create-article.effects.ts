0e6189da781412ce48dc8537f89bf89da079a184
Gregory Pereverzev <boot762@gmail.com>
2024-05-28 10:45:32 +0300

    feat: add CreateArticleEffects

---
+++ src/app/create-article/store/create-article.effects.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { createEffect, ofType, Actions } from '@ngrx/effects';
+import { switchMap, map, catchError, of, tap } from 'rxjs';
+
+import { ArticleInterface } from '../../shared/types/article.interface';
+import { CreateArticleService } from '../services/create-article.service';
+import { createArticleActions } from './create-article.actions';
+
+@Injectable()
+export class CreateArticleEffects {
+  createArticle$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(createArticleActions.createArticle),
+      switchMap(({ articleInput }) => {
+        return this.createArticleService.createArticle(articleInput).pipe(
+          map((article: ArticleInterface) => {
+            return createArticleActions.createArticleSuccess({ article });
+          }),
+          catchError((errorResponse: HttpErrorResponse) =>
+            of(
+              createArticleActions.createArticleFailure({
+                errors: errorResponse.error.errors,
+              })
+            )
+          )
+        );
+      })
+    )
+  );
+
+  redirectAfterCreate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(createArticleActions.createArticleSuccess),
+        tap(({ article }) => {
+          this.router.navigate(['/article', article.slug]);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private createArticleService: CreateArticleService,
+    private router: Router
+  ) {}
+}
