6cec232cee29a13fc77338f0a87eefe03eef576d
Gregory Pereverzev <boot762@gmail.com>
2024-05-30 00:45:26 +0300

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { TagFeedComponent } from './tag-feed/components/tag-feed.component';
 import { ArticleComponent } from './article/components/article.component';
 import { CreateArticleComponent } from './create-article/components/create-article/create-article.component';
 import { EditArticleComponent } from './edit-article/components/edit-article/edit-article.component';
+import { UserSettingsComponent } from './user-settings/components/user-settings/user-setting.component';
 
 export const routes: Routes = [
   { path: '', component: GlobalFeedComponent },
@@ -15,6 +16,7 @@ export const routes: Routes = [
   { path: 'tags/:slug', component: TagFeedComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'settings', component: UserSettingsComponent },
   { path: 'articles/new', component: CreateArticleComponent },
   { path: 'articles/:slug/edit', component: EditArticleComponent },
   { path: 'articles/:slug', component: ArticleComponent },
