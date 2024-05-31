fbe8e1a9d0073dcbe0852bc68d40d0c64eb5eedf
Gregory Pereverzev <boot762@gmail.com>
2024-05-31 08:49:43 +0300

    feat: add AddToFavoritesComponent

---
+++ src/app/shared/add-to-favorites/components/add-to-favorites/add-to-favorites.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'mc-add-to-favorites',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-to-favorites.component.html',
+  styleUrl: './add-to-favorites.component.scss',
+})
+export class AddToFavoritesComponent {}
