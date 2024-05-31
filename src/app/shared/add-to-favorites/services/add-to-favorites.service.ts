6dd49674a1c0b57ca2bc41bad76dc36c08859b1d
Gregory Pereverzev <boot762@gmail.com>
2024-05-31 09:22:50 +0300

    feat: add AddToFavoreitesService

---
+++ src/app/shared/add-to-favorites/services/add-to-favorites.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
+
+import { ArticleInterface } from '../../types/article.interface';
+import { GetArticleResponseInterface } from '../../types/get-article-response.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AddToFavoreitesService {
+  constructor(private http: HttpClient) {}
+
+  toFavorite(slug: string): Observable<ArticleInterface> {
+    return this.http
+      .post<GetArticleResponseInterface>(`/favorite/${slug}/favorite`, {})
+      .pipe(map(response => response.article));
+  }
+
+  toUnfavorite(slug: string): Observable<ArticleInterface> {
+    return this.http
+      .delete<GetArticleResponseInterface>(`/favorite/${slug}/favorite`)
+      .pipe(map(response => response.article));
+  }
+}
