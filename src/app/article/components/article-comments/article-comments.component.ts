b8d78f5ec97823bbb11234665cc4dcfb460c0447
Gregory Pereverzev <boot762@gmail.com>
2024-06-06 19:18:45 +0300

    feat: add ArticleCommentsComponent

---
+++ src/app/article/components/article-comments/article-comments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-article-comments',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-comments.component.html',
+  styleUrl: './article-comments.component.scss',
+})
+export class ArticleCommentsComponent {}
