d19665eefc9563177b0b518fd35f0589aa13439d
Gregory Pereverzev <boot762@gmail.com>
2025-01-12 13:52:11 +0300

    fix: hide the "delete" button in CommentCardComponent if the current user is not the author of the comment

--- src/app/article/components/comment-card/comment-card.component.ts
+++ src/app/article/components/comment-card/comment-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 import { CommentInterface } from '../../types/comment.interface';
 import { articleCommentsActions } from '../../store/actions/article-comments.actions';
 import { DatePipe } from '@angular/common';
 import { ConstantsEnum } from '../../../shared/enums/constants.enum';
+import { authFeature } from '../../../auth/store/auth.state';
 
 @Component({
   selector: 'mc-comment-card',
@@ -14,13 +16,29 @@ import { ConstantsEnum } from '../../../shared/enums/constants.enum';
   templateUrl: './comment-card.component.html',
   styleUrl: './comment-card.component.scss',
 })
-export class CommentCardComponent {
+export class CommentCardComponent implements OnInit, OnDestroy {
   @Input('comment') commentProps!: CommentInterface;
   @Input('articleSlug') articleSlugProps!: string;
 
   store = inject(Store);
 
+  currentUserSubscription!: Subscription;
+  isCommentAuthor!: boolean;
+
   readonly defaultUserImage = ConstantsEnum.DEFAULT_USER_IMAGE as string;
+
+  initializeValues(): void {
+    this.currentUserSubscription = this.store
+      .select(authFeature.selectCurrentUser)
+      .subscribe(user => {
+        this.isCommentAuthor =
+          user?.username === this.commentProps.author.username;
+      });
+  }
+
+  ngOnInit(): void {
+    this.initializeValues();
+  }
 
   deleteComment(): void {
     this.store.dispatch(
@@ -29,5 +47,9 @@ export class CommentCardComponent {
         commentId: this.commentProps.id,
       })
     );
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
   }
 }
