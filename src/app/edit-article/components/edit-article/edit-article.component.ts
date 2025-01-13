35feea1301b76fa47745f0fdcedfc8df35a51c90
Gregory Pereverzev <boot762@gmail.com>
2025-01-13 15:49:49 +0300

    feat: add comments to EditArticleComponent

--- src/app/edit-article/components/edit-article/edit-article.component.ts
+++ src/app/edit-article/components/edit-article/edit-article.component.ts
@@ -12,6 +12,10 @@ import { editArticleFeature } from '../../store/edit-article.state';
 import { editArticleActions } from '../../store/edit-article.actions';
 import { LoadingComponent } from '../../../shared/loading/loading.component';
 
+/**
+ * EditArticleComponent is responsible for displaying the form to edit an existing article.
+ * It fetches the article data from the store based on the slug, and allows users to submit updated article data.
+ */
 @Component({
   selector: 'mc-edit-article',
   standalone: true,
@@ -20,26 +24,53 @@ import { LoadingComponent } from '../../../shared/loading/loading.component';
   styleUrl: './edit-article.component.scss',
 })
 export class EditArticleComponent implements OnInit, OnDestroy {
+  /** The slug of the article to be edited */
   public slug!: string | null;
+
+  /** Indicates whether the form is currently submitting */
   public isSubmitting$!: Observable<boolean>;
+
+  /** Indicates whether the form is currently loading */
   public isLoading$!: Observable<boolean>;
+
+  /** The backend validation errors */
   public backendErrors$!: Observable<BackendErrorsInterface | null>;
+
+  /** The initial values for the form */
   public initialValues!: ArticleInputInterface;
+
+  /** The subscription to the initialValues observable */
   public initialValuesSubscription!: Subscription;
 
   constructor(private store: Store, private route: ActivatedRoute) {}
 
+  /**
+   * Lifecycle hook for component initialization.
+   * Initializes values and fetches the article data based on the slug from the route.
+   */
   ngOnInit(): void {
     this.initializeValues();
     this.fetchData();
   }
 
+  /**
+   * Dispatches an action to fetch the article data based on the slug from the route.
+   * The action is only dispatched if the slug is available.
+   */
   fetchData(): void {
     if (this.slug) {
       this.store.dispatch(editArticleActions.getArticle({ slug: this.slug }));
     }
   }
 
+  /**
+   * Initializes the Observables for component data, and the initial values for the form.
+   * The `isSubmitting$` observable tracks the state of the article form submission.
+   * The `isLoading$` observable tracks the state of the article loading.
+   * The `backendErrors$` observable holds any backend validation errors that may have occurred.
+   * The `initialValues` property is used to initialize the form with the existing article data.
+   * @returns void
+   */
   initializeValues(): void {
     this.slug = this.route.snapshot.paramMap.get('slug');
     this.isSubmitting$ = this.store.select(
@@ -61,6 +92,12 @@ export class EditArticleComponent implements OnInit, OnDestroy {
       });
   }
 
+  /**
+   * Dispatches an action to update the article based on the slug from the route.
+   * The action is only dispatched if the slug is available.
+   * @param articleInput The input data for the article form.
+   * @returns void
+   */
   onSubmit(articleInput: ArticleInputInterface) {
     if (this.slug) {
       this.store.dispatch(
@@ -72,6 +109,11 @@ export class EditArticleComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Lifecycle hook for component destruction.
+   * Unsubscribes from the `initialValues` subscription to prevent memory leaks.
+   * @returns void
+   */
   ngOnDestroy(): void {
     this.initialValuesSubscription.unsubscribe();
   }
