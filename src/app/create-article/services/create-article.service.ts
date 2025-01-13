b597a2d58e7b735a8fa79d9d992e3e57cb0ef6f6
Gregory Pereverzev <boot762@gmail.com>
2025-01-13 15:42:40 +0300

    feat: add comments to CreateArticleService

--- src/app/create-article/services/create-article.service.ts
+++ src/app/create-article/services/create-article.service.ts
@@ -13,6 +13,11 @@ import { SaveArticleInterface } from '../../shared/types/save-article-response.i
 export class CreateArticleService {
   constructor(private http: HttpClient) {}
 
+  /**
+   * Submits a new article to the API to be created.
+   * @param articleInput The input data for the article form.
+   * @returns An Observable of the created article, or an error if the submission fails.
+   */
   createArticle(
     articleInput: ArticleInputInterface
   ): Observable<ArticleInterface> {
