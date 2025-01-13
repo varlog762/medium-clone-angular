b6ed1c277de9ff5e10de9640142f42345141d347
Gregory Pereverzev <boot762@gmail.com>
2025-01-13 15:24:55 +0300

    feat: add comments to RegisterComponent

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -11,6 +11,17 @@ import { BackendErrorsInterface } from '../../../shared/types/backend-errors.int
 import { BackendErrorMessagesComponent } from '../../../shared/backend-error-messages/components/backend-error-messages.component';
 import { authActions } from '../../store/auth.actions';
 
+/**
+ * This component represents the user registration page.
+ * It provides:
+ * - A form with fields for username, email, and password.
+ * - Validation error messages if registration fails.
+ * - A link to navigate to the login page for existing users.
+ *
+ * The component uses:
+ * - Reactive forms for managing form state.
+ * - NgRx for state management and handling registration actions.
+ */
 @Component({
   selector: 'mc-register',
   standalone: true,
@@ -25,17 +36,31 @@ import { authActions } from '../../store/auth.actions';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent implements OnInit {
+  /** Reactive form group for managing the registration form */
   public form!: FormGroup;
+
+  /** Observable for tracking form submission status */
   public isSubmitting$!: Observable<boolean>;
+
+  /** Observable for tracking backend validation errors */
   public backendErrors$!: Observable<BackendErrorsInterface | null>;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
+  /**
+   * Initializes the component.
+   * - Sets up the reactive form for registration.
+   * - Initializes the Observables for tracking form submission status and backend validation errors.
+   */
   ngOnInit(): void {
     this.initializeForm();
     this.initializeValues();
   }
 
+  /**
+   * Initializes the Observables for tracking form submission status
+   * and backend validation errors.
+   */
   initializeValues(): void {
     this.isSubmitting$ = this.store.pipe(
       select(authFeature.selectIsSubmitting)
@@ -45,6 +70,13 @@ export class RegisterComponent implements OnInit {
     );
   }
 
+  /**
+   * Initializes the reactive form for registration.
+   * - Creates a form group with the following fields:
+   *   - username: empty string
+   *   - email: empty string
+   *   - password: empty string
+   */
   initializeForm(): void {
     this.form = this.fb.group({
       username: '',
@@ -53,6 +85,11 @@ export class RegisterComponent implements OnInit {
     });
   }
 
+  /**
+   * Submits the registration form.
+   * - Creates a request object with the username, email, and password from the form.
+   * - Dispatches the register action with the request object.
+   */
   onSubmit(): void {
     const request: RegisterRequestInterface = {
       user: this.form.value,
