fce179921107b11985729784cc613a97b28e40d9
Gregory Pereverzev <boot762@gmail.com>
2025-01-14 14:10:33 +0300

    feat: add comments to PaginationComponent and UtilsService

--- src/app/global-feed/components/pagination/pagination.component.ts
+++ src/app/global-feed/components/pagination/pagination.component.ts
@@ -5,6 +5,10 @@ import { NgClass } from '@angular/common';
 import { UtilsService } from '../../../shared/services/utils.service';
 import { environment } from '../../../../environments/environment.development';
 
+/**
+ * The PaginationComponent handles the display and navigation of pagination controls.
+ * It calculates the total pages based on input data and highlights the current page.
+ */
 @Component({
   selector: 'mc-pagination',
   standalone: true,
@@ -13,17 +17,33 @@ import { environment } from '../../../../environments/environment.development';
   styleUrl: './pagination.component.scss',
 })
 export class PaginationComponent implements OnInit {
+  /** The total number of articles */
   @Input('total') total!: number | undefined;
+
+  /** The number of articles per page */
   @Input('limit') limit!: number;
+
+  /** The base URL for generating pagination links */
   @Input('url') url!: string;
+
+  /** The current page number */
   @Input('currentPage') currentPage!: number;
 
+  /** The starting page number */
   public startPage!: number;
+
+  /** The total number of pages */
   public pagesCount!: number;
+
+  /** An array of page numbers */
   public pages!: number[];
 
   constructor(private utilsService: UtilsService) {}
 
+  /**
+   * Lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+   * Calculates the total number of pages and generates an array of page numbers.
+   */
   ngOnInit(): void {
     if (this.total) {
       this.pagesCount = Math.ceil(this.total / this.limit);
--- src/app/shared/services/utils.service.ts
+++ src/app/shared/services/utils.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UtilsService {
+  /**
+   * Creates an array of numbers from start to end (inclusive)
+   * @example range(1, 3) => [1, 2, 3]
+   * @param {number} start - starting number
+   * @param {number} end - end number
+   * @returns {number[]} array of numbers from start to end
+   */
   range(start: number, end: number): number[] {
     return [...Array(end).keys()].map(elem => elem + start);
   }
