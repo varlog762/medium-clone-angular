7e71875ace54f80f579bf6a32f1d6db72e068ce5
Gregory Pereverzev <boot762@gmail.com>
2025-01-14 15:15:25 +0300

    feat: add comments to UserProfileComponent

--- src/app/user-profile/components/user-profile/user-profile.component.ts
+++ src/app/user-profile/components/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import {
   ActivatedRoute,
@@ -20,6 +20,12 @@ import { LoadingComponent } from '../../../shared/loading/loading.component';
 import { AddToFollowComponent } from '../../../shared/add-to-follow/components/add-to-follow/add-to-follow.component';
 import { ConstantsEnum } from '../../../shared/enums/constants.enum';
 
+/**
+ * The UserProfileComponent is responsible for displaying information about a user's profile.
+ * It includes the user's details (username, bio, profile image) and their articles.
+ * Additionally, it allows navigation between the user's articles and their favorited articles.
+ * The component also handles the logic for following/unfollowing users.
+ */
 @Component({
   selector: 'mc-user-profile',
   standalone: true,
@@ -35,29 +41,68 @@ import { ConstantsEnum } from '../../../shared/enums/constants.enum';
   styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent implements OnInit, OnDestroy {
+  /** Dependencies */
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+  store = inject(Store);
+
+  /** Holds the slug (username) of the profile being viewed. */
   slug!: string;
+
+  /** Stores the user's profile data. */
   userProfile!: ProfileInterface;
+
+  /** Subscription for profile data updates. */
   userProfileSubscription!: Subscription;
+
+  /** Indicates whether the current user is following the profile being viewed. */
   isFollowed!: boolean;
+
+  /**  Indicates whether the profile data is currently being loaded. */
   isLoading$!: Observable<boolean>;
+
+  /** Indicates whether there was an error fetching the profile data. */
   error$!: Observable<string | null>;
+
+  /** The API endpoint for fetching user profile data. */
   apiUrl!: string;
+
+  /** Observable that indicates whether the current user is the profile being viewed. */
   isCurrentUserProfile$!: Observable<boolean>;
-  routParamsSubscription!: Subscription;
+
+  /** Subscription for route parameters updates. */
+  routeParamsSubscription!: Subscription;
+
+  /** Observable that indicates whether the current user is logged in. */
   isLoggedIn$!: Observable<boolean | null>;
-  defaultUserImage = ConstantsEnum.DEFAULT_USER_IMAGE;
 
-  constructor(
-    private route: ActivatedRoute,
-    private store: Store,
-    private router: Router
-  ) {}
+  /** Default user image URL. */
+  readonly defaultUserImage = ConstantsEnum.DEFAULT_USER_IMAGE;
 
+  /**
+   * Lifecycle hook that is called after Angular has initialized all data-bound
+   * properties of a directive.
+   *
+   * This method is used to initialize the component's state and subscribe to
+   * route parameter changes.
+   *
+   * @returns void
+   */
   ngOnInit(): void {
     this.initializeValues();
     this.initializeListeners();
   }
 
+  /**
+   * Initializes the component's state.
+   *
+   * This method is used to set the `slug` property to the value of the route
+   * parameter `slug`, and to initialize the Observables for tracking the
+   * loading state, error state and whether the current user is the profile
+   * being viewed.
+   *
+   * @returns void
+   */
   initializeValues(): void {
     this.slug = this.route.snapshot.paramMap.get('slug') as string;
     this.isLoggedIn$ = this.store.select(authFeature.selectIsLoggedIn);
@@ -84,8 +129,18 @@ export class UserProfileComponent implements OnInit, OnDestroy {
     );
   }
 
+  /**
+   * Initializes the component's event listeners.
+   *
+   * This method is used to subscribe to changes in the route parameters and
+   * to changes in the current user's profile. It updates the component's state
+   * when the route parameters are changed, and when the current user's profile
+   * is updated.
+   *
+   * @returns void
+   */
   initializeListeners(): void {
-    this.routParamsSubscription = this.route.params.subscribe(
+    this.routeParamsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.slug = params['slug'];
         this.fetchUserProfile();
@@ -99,10 +154,28 @@ export class UserProfileComponent implements OnInit, OnDestroy {
       });
   }
 
+  /**
+   * Dispatches an action to fetch the user profile data from the API.
+   *
+   * This method is used to fetch the user profile data when the component is
+   * initialized or when the route parameters are changed.
+   *
+   * @returns void
+   */
   fetchUserProfile(): void {
     this.store.dispatch(userProfileActions.getUserProfile({ slug: this.slug }));
   }
 
+  /**
+   * Constructs the API URL for fetching the user's articles.
+   *
+   * The API endpoint depends on whether the user is viewing their own
+   * articles or their favorited articles. If the user is viewing their
+   * favorited articles, the API endpoint is `/articles?favorited=${this.slug}`.
+   * Otherwise, the API endpoint is `/articles?author=${this.slug}`.
+   *
+   * @returns The API URL for fetching the user's articles.
+   */
   getApiUrl(): string {
     const isFavorited = this.router.url.includes('favorites');
     return (this.apiUrl = isFavorited
@@ -110,8 +183,14 @@ export class UserProfileComponent implements OnInit, OnDestroy {
       : `/articles?author=${this.slug}`);
   }
 
+  /**
+   * Cleans up the component by unsubscribing from the route parameter
+   * changes and the user profile data when the component is destroyed.
+   *
+   * @returns void
+   */
   ngOnDestroy(): void {
-    this.routParamsSubscription.unsubscribe();
+    this.routeParamsSubscription.unsubscribe();
     this.userProfileSubscription.unsubscribe();
   }
 }
