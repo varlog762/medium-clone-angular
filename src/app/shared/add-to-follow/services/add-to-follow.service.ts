9844e0a6c54d215946078b174829e68273f01cb8
Gregory Pereverzev <boot762@gmail.com>
2025-01-15 14:12:41 +0300

    feat: add comments to AddToFollowService

--- src/app/shared/add-to-follow/services/add-to-follow.service.ts
+++ src/app/shared/add-to-follow/services/add-to-follow.service.ts
@@ -11,18 +11,36 @@ import { GetProfileResponseInterface } from '../../../user-profile/types/get-pro
 export class AddToFollowService {
   constructor(private http: HttpClient) {}
 
+  /**
+   * Makes a POST request to the API to follow the user with the given slug.
+   *
+   * @param slug The slug of the user to follow.
+   * @returns An observable of the user's updated profile.
+   */
   followUser(slug: string): Observable<ProfileInterface> {
     return this.http
       .post<GetProfileResponseInterface>(this.getApiUrl(slug), {})
       .pipe(map((response: GetProfileResponseInterface) => response.profile));
   }
 
+  /**
+   * Makes a DELETE request to the API to unfollow the user with the given slug.
+   *
+   * @param slug The slug of the user to unfollow.
+   * @returns An observable of the user's updated profile.
+   */
   unfollowUser(slug: string): Observable<ProfileInterface> {
     return this.http
       .delete<GetProfileResponseInterface>(this.getApiUrl(slug))
       .pipe(map((response: GetProfileResponseInterface) => response.profile));
   }
 
+  /**
+   * Constructs the API URL for the follow/unfollow endpoint.
+   *
+   * @param slug The slug of the user to follow/unfollow.
+   * @returns The API URL for the follow/unfollow endpoint.
+   */
   getApiUrl(slug: string): string {
     return `/profiles/${slug}/follow`;
   }
