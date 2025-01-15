0e7bca3adf11d96bd5f9583afd0fe86f248b75d2
Gregory Pereverzev <boot762@gmail.com>
2025-01-15 13:54:10 +0300

    feat: add comments to AddToFavoritesService

--- src/app/shared/add-to-favorites/services/add-to-favorites.service.ts
+++ src/app/shared/add-to-favorites/services/add-to-favorites.service.ts
@@ -12,11 +12,27 @@ import { GetArticleResponseInterface } from '../../types/get-article-response.in
 export class AddToFavoritesService {
   constructor(private http: HttpClient) {}
 
+  /**
+   * Toggles the favorite status of an article with the given slug.
+   * If the article was not favorited before, it will be added to the user's favorites.
+   * If the article was already favorited, it will be removed from the user's favorites.
+   *
+   * @param slug The slug of the article to be toggled.
+   * @returns The updated article object.
+   */
   addToFavorites(slug: string): Observable<ArticleInterface> {
     return this.http
       .post<GetArticleResponseInterface>(`/articles/${slug}/favorite`, {})
       .pipe(map(response => response.article));
   }
+
+  /**
+   * Removes the favorite status of an article with the given slug.
+   * This will unfavorite the article if it was previously favorited by the user.
+   *
+   * @param slug The unique identifier of the article to be unfavorited.
+   * @returns An observable of the updated article object.
+   */
 
   removeFromFavorites(slug: string): Observable<ArticleInterface> {
     return this.http
