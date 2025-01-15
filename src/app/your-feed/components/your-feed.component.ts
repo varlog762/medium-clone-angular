6bb91918d22193565aba718f420ac5050d959e37
Gregory Pereverzev <boot762@gmail.com>
2025-01-15 13:37:06 +0300

    feat: add comments to YourFeedComponent

--- src/app/your-feed/components/your-feed.component.ts
+++ src/app/your-feed/components/your-feed.component.ts
@@ -5,6 +5,10 @@ import { BannerComponent } from '../../shared/banner/components/banner.component
 import { PopularTagsComponent } from '../../shared/popular-tags/components/popular-tags.component';
 import { FeedTogglerComponent } from '../../shared/feed-toggler/components/feed-toggler.component';
 
+/**
+ * Component responsible for displaying the feed of articles authored by users
+ * that the current user is following. This component is accessible only to authenticated users.
+ */
 @Component({
   selector: 'mc-your-feed',
   standalone: true,
@@ -18,5 +22,9 @@ import { FeedTogglerComponent } from '../../shared/feed-toggler/components/feed-
   styleUrl: './your-feed.component.scss',
 })
 export class YourFeedComponent {
-  public apiUrl = '/articles/feed';
+  /**
+   * The API endpoint for retrieving the feed of articles from authors
+   * followed by the current user.
+   */
+  apiUrl = '/articles/feed';
 }
