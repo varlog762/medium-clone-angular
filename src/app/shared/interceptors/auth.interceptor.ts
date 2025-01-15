2b856e92e1eab3544266ddd958015b815af98ba5
Gregory Pereverzev <boot762@gmail.com>
2025-01-15 15:05:04 +0300

    feat: add comments to interceptors

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -3,6 +3,11 @@ import { inject } from '@angular/core';
 
 import { PersistenceService } from '../services/persistence.service';
 
+/**
+ * HttpInterceptor that adds Authorization header with token to all requests.
+ * It uses {@link PersistenceService} to get the token from local storage.
+ * If token is not found, the header is not added.
+ */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const persistenceService = inject(PersistenceService);
   const token = persistenceService.get('accessToken');
--- src/app/shared/interceptors/url.interceptor.ts
+++ src/app/shared/interceptors/url.interceptor.ts
@@ -1,5 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
+/**
+ * Adds base URL to the request URL.
+ *
+ * Modifies the request by adding a base URL to the request URL.
+ * The base URL is the URL of the API endpoint that the request is being sent to.
+ * The `url` property is modified in-place.
+ *
+ * @param req The request.
+ * @param next The next interceptor in the chain.
+ * @returns The modified request.
+ */
 export const urlInterceptor: HttpInterceptorFn = (req, next) => {
   const baseUrl: string = 'https://api.greg-p.keenetic.pro/api';
   // const baseUrl: string = 'https://conduit-realworld-example-app.fly.dev/api';
