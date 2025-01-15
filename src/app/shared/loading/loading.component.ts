a807fd1652b87fc0c4f3a6c12a14eceb404c8515
Gregory Pereverzev <boot762@gmail.com>
2025-01-15 15:07:47 +0300

    feat: add comments to LoadingComponent

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+/**
+ * Component for displaying a loading spinner.
+ */
 @Component({
   selector: 'mc-loading',
   standalone: true,
   imports: [],
   templateUrl: './loading.component.html',
-  styleUrl: './loadnig.component.scss',
+  styleUrl: './loading.component.scss',
 })
 export class LoadingComponent {}
